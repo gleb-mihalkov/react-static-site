247ddf110c9efcb6d586cba77b7e7219870c12a0
Глеб Михальков <gleb.mihalkov@devim.team>
2019-07-31 12:07:25 +0300

    Implement isComponentPrerendered

--- src/common/helpers/isComponentPrerendered.ts
+++ src/common/helpers/isComponentPrerendered.ts
@@ -1,8 +1,8 @@
 /**
  * Returns true if the React DOM has been prerendered.
- * @todo Implement.
  * @param rootNode Mounting point of the application.
  */
 export const isComponentPrerendered = (rootNode: HTMLElement) => {
-  return false;
+  const isElementNode = (node: ChildNode) => node.nodeType === Node.ELEMENT_NODE;
+  return Array.prototype.some.call(rootNode.childNodes, isElementNode);
 };
